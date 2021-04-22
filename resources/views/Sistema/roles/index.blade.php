e2dc75d9aacdc9a296d9dceedcc1518d6eb82ab1
BernaGN <31524330+BernaGN@users.noreply.github.com>
2021-04-22 18:23:52 -0500

    Cambio titulo

--- resources/views/Sistema/roles/index.blade.php
+++ resources/views/Sistema/roles/index.blade.php
@@ -102,7 +102,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Agregar Usuario</h4>
+                    <h4 class="modal-title">Agregar Rol</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
